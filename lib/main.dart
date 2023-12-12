6b33f9b31d1ef3148049df677bda06f202503204
c-bv <charles.bouveret.dev@gmail.com>
2023-12-13 00:55:12 +0100

    Delete unused files and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:rss_feed_reader_app/screens/home.dart';
+import 'package:rss_feed_reader_app/src/screens/home_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,7 +22,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const HomePage()
-    );
+        home: const HomePage());
   }
 }
