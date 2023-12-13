0292f189b74fb302142962e7c394dedd577ea870
c-bv <charles.bouveret.dev@gmail.com>
2023-12-13 22:44:51 +0100

    Add animated splash screen and import necessary packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:rss_feed_reader_app/src/config/firebase_options.dart';
 import 'package:rss_feed_reader_app/src/screens/home_screen.dart';
 import 'package:rss_feed_reader_app/src/screens/login_screen.dart';
 import 'package:rss_feed_reader_app/src/services/auth_service.dart';
-
-import 'src/config/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,16 +29,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: ThemeData.dark(),
-      home: StreamBuilder<User?>(
-        stream: _authService.authStateChanges,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else {
-            return _authService.isUserLoggedIn ? HomeScreen() : const LoginScreen();
-          }
-        },
+      home: AnimatedSplashScreen(
+        splash: const FlutterLogo(size: 100),
+        duration: 3000,
+        nextScreen: StreamBuilder<User?>(
+          stream: _authService.authStateChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return _authService.isUserLoggedIn
+                  ? HomeScreen()
+                  : const LoginScreen();
+            }
+          },
+        ),
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: Colors.white,
       ),
     );
   }
